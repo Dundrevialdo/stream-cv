529499d8afdf6c1bb33f75ddeb0c28106849fa11
Krystian Czarnecki <71127494+Dundrevialdo@users.noreply.github.com>
2024-03-08 23:38:45 +0100

    src/links.py: Add links

--- src/links.py
+++ src/links.py
@@ -18,6 +18,22 @@ class Link:
         'url': 'https://docs.opencv.org/4.5.2/dd/d1a/group__imgproc__feature.html#ga04723e007ed888ddf11d9ba04e2232de',
         'label': 'cv2.Canny()'
     }
+    filter2d = {
+        'url': 'https://docs.opencv.org/4.5.2/d4/d86/group__imgproc__filter.html#ga27c049795ce870216ddfb366086b5a04',
+        'label': 'cv2.filter2D()'
+    }
+    convert_scale_abs = {
+        'url': 'https://docs.opencv.org/4.5.2/d2/de8/group__core__array.html#ga3460e9c9f37b563ab9dd550c4d8c4e7d',
+        'label': 'cv2.convertScaleAbs()'
+    }
+    pencil_sketch = {
+        'url': 'https://docs.opencv.org/4.5.2/df/dac/group__photo__render.html#gae5930dd822c713b36f8529b21ddebd0c',
+        'label': 'cv2.pencilSketch()'
+    }
+    stylization = {
+        'url': 'https://docs.opencv.org/4.5.2/df/dac/group__photo__render.html#gacb0f7324017df153d7b5d095aed53206',
+        'label': 'cv2.stylization()'
+    }
 
     @staticmethod
     def show(link_name):
