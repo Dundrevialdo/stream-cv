dcfa29ec9716786faa1f916bd4d31066b2d5f832
Krystian Czarnecki <71127494+Dundrevialdo@users.noreply.github.com>
2024-03-08 21:11:33 +0100

    .\pages\9_About.py: Upadte

--- pages/9_About.py
+++ pages/9_About.py
@@ -5,5 +5,5 @@ st.title('About')
 st.write("""The app builds on top of OpenCV application introduced in 'Mastering OpenCV with Python' course. 
 The course is available in OpenCV.org and is a great resource for learning OpenCV.""")
 
-st.write('Co-Author:')
-st.write('Krystian Czarnecki')
+st.write('Co-Author: Krystian Czarnecki')
+st.markdown('[LinkedIn](https://www.linkedin.com/in/krystian-czarnecki-47458b180/)')
