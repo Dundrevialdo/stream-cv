5cb7224ea2cc21947a20ef3f81b738fd4f6850b1
Krystian Czarnecki <71127494+Dundrevialdo@users.noreply.github.com>
2024-03-08 23:19:50 +0100

    Filters.py: Add filters and refactor

--- Filters.py
+++ Filters.py
@@ -1,90 +1,73 @@
-import io
-import base64
-
-import streamlit as st
-import cv2
-
-from PIL import Image
-
 from src.filters import *
 from src.common import *
 
-
-# Generating a link to download a particular image file.
-
-
-
-# Set title.
 st.title('Artistic Image Filters')
 
-# Upload image.
 uploaded_file = st.file_uploader('Choose an image file:', type=['png', 'jpg'])
 
-if uploaded_file is not None:
-    # Convert the file to an opencv image.
-    raw_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    img = cv2.imdecode(raw_bytes, cv2.IMREAD_COLOR)
-    input_col, output_col = st.columns(2)
-    with input_col:
-        st.header('Original')
-        # Display uploaded image.
-        st.image(img, channels='BGR', use_column_width=True)
+st.header('Filter Examples')
 
-    st.header('Filter Examples:')
-    # Display a selection box for choosing the filter to apply.
-    option = st.selectbox('Select a filter:',
-                          ('None',
-                           'Black and White',
-                           'Sepia / Vintage',
-                           'Vignette Effect',
-                           'Pencil Sketch',
-                           ))
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.caption('Black and White')
+    st.image('gallery/filter_bw.jpg')
+with col2:
+    st.caption('Sepia / Vintage')
+    st.image('gallery/filter_sepia.jpg')
+with col3:
+    st.caption('Vignette Effect')
+    st.image('gallery/filter_vignette.jpg')
+with col4:
+    st.caption('Pencil Sketch')
+    st.image('gallery/filter_pencil_sketch.jpg')
 
-    # Define columns for thumbnail images.
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        st.caption('Black and White')
-        st.image('gallery/filter_bw.jpg')
-    with col2:
-        st.caption('Sepia / Vintage')
-        st.image('gallery/filter_sepia.jpg')
-    with col3:
-        st.caption('Vignette Effect')
-        st.image('gallery/filter_vignette.jpg')
-    with col4:
-        st.caption('Pencil Sketch')
-        st.image('gallery/filter_pencil_sketch.jpg')
+if not uploaded_file:
+    st.stop()
 
-    # Flag for showing output image.
-    output_flag = 1
-    # Colorspace of output image.
-    color = 'BGR'
+img = load_image(uploaded_file)
+output_img = img.copy()
 
-    # Generate filtered image based on the selected option.
-    if option == 'None':
-        # Don't show output image.
-        output_flag = 0
+option = st.selectbox('Select a filter:',
+                      ('None',
+                       'Brighten / Darken',
+                       'Black and White',
+                       'Sepia / Vintage',
+                       'Vignette Effect',
+                       '3D Embossed Effect',
+                       'Outline Effect',
+                       'Pencil Sketch',
+                       'Stylization Effect'
+                       ))
+
+if option != 'None':
+    if option == 'Brighten / Darken':
+        output_img = brightness_and_contrast(output_img)
     elif option == 'Black and White':
-        output = bw_filter(img)
-        color = 'GRAY'
+        output_img = bw_filter(output_img)
     elif option == 'Sepia / Vintage':
-        output = sepia(img)
+        output_img = sepia(output_img)
     elif option == 'Vignette Effect':
-        level = st.slider('level', 0, 5, 2)
-        output = vignette(img, level)
+        intensity = st.slider('Intensity', 1, 5)
+        output_img = vignette(output_img, intensity)
+    elif option == '3D Embossed Effect':
+        output_img = embossed_edges(output_img)
+    elif option == 'Outline Effect':
+        output_img = outline_effect(output_img)
     elif option == 'Pencil Sketch':
-        ksize = st.slider('Blur kernel size', 1, 11, 5, step=2)
-        output = pencil_sketch(img, ksize)
-        color = 'GRAY'
+        output_img = pencil_sketch(output_img)
+    elif option == 'Stylization Effect':
+        output_img = stylization_effect(output_img)
 
-    with output_col:
-        if output_flag == 1:
-            st.header('Output')
-            st.image(output, channels=color)
-            # fromarray convert cv2 image into PIL format for saving it using download link.
-            if color == 'BGR':
-                result = Image.fromarray(output[:, :, ::-1])
-            else:
-                result = Image.fromarray(output)
-            # Display link.
-            show_image_download_button(result)
+input_col, output_col = st.columns(2)
+with input_col:
+    st.header('Original')
+    # Display uploaded image.
+    st.image(img, channels='BGR', use_column_width=True)
+with output_col:
+    st.header('Output')
+    if output_img.ndim == 3:
+        output_img = output_img[..., ::-1]
+    st.image(output_img)
+
+    result = Image.fromarray(output_img)
+    show_image_download_button(result)
--- src/filters.py
+++ src/filters.py
@@ -3,31 +3,16 @@ import numpy as np
 import streamlit as st
 
 
+def brightness_and_contrast(img):
+    brightness = st.slider('Brightness', -100, 100, 0)
+    contrast = st.slider('Contrast', 0.0, 5.0, 1.0, 0.01)
+    return cv2.convertScaleAbs(img, alpha=contrast, beta=brightness)
+
+
 @st.cache_data
 def bw_filter(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img_gray
-
-
-@st.cache_data
-def vignette(img, level=2):
-    height, width = img.shape[:2]
-
-    # Generate vignette mask using Gaussian kernels.
-    X_resultant_kernel = cv2.getGaussianKernel(width, width / level)
-    Y_resultant_kernel = cv2.getGaussianKernel(height, height / level)
-
-    # Generating resultant_kernel matrix.
-    kernel = Y_resultant_kernel * X_resultant_kernel.T
-    mask = kernel / kernel.max()
-
-    img_vignette = np.copy(img)
-
-    # Apply the mask to each channel in the input image.
-    for i in range(3):
-        img_vignette[:, :, i] = img_vignette[:, :, i] * mask
-
-    return img_vignette
 
 
 @st.cache_data
@@ -46,7 +31,61 @@ def sepia(img):
 
 
 @st.cache_data
-def pencil_sketch(img, ksize=5):
-    img_blur = cv2.GaussianBlur(img, (ksize, ksize), 0, 0)
-    img_sketch, _ = cv2.pencilSketch(img_blur)
-    return img_sketch
+def vignette(img, level):
+    height, width = img.shape[:2]
+
+    # Generate vignette mask using Gaussian kernels.
+    X_resultant_kernel = cv2.getGaussianKernel(width, width / level)
+    Y_resultant_kernel = cv2.getGaussianKernel(height, height / level)
+
+    # Generating resultant_kernel matrix.
+    kernel = Y_resultant_kernel * X_resultant_kernel.T
+    mask = kernel / kernel.max()
+
+    img_vignette = np.copy(img)
+
+    # Apply the mask to each channel in the input image.
+    for i in range(img_vignette.ndim):
+        img_vignette[..., i] = img_vignette[..., i] * mask
+
+    return img_vignette
+
+
+@st.cache_data
+def embossed_edges(img):
+    kernel = np.array([[0, -3, -3],
+                       [3, 0, -3],
+                       [3, 3, 0]])
+    return cv2.filter2D(img, -1, kernel=kernel)
+
+
+# may have better results with blurred image
+# may look better with a greyscale image
+def outline_effect(img):
+    k = st.slider('Outline Intensity', 8, 15, 9, 1)
+    kernel = np.array([[-1, -1, -1],
+                       [-1, k, -1],
+                       [-1, -1, -1]])
+    return cv2.filter2D(img, ddepth=-1, kernel=kernel)
+
+
+# may have better results with blurred image
+def pencil_sketch(img):
+    sketch_option = st.radio('Pencil Sketch Type', ('Grayscale', 'Color'), horizontal=True)
+    sigma_s = st.slider('Sigma S', 0.0, 200.0, 60.0)
+    sigma_r = st.slider('Sigma R', 0.0, 1.0, 0.45, 0.01)
+    shade_factor = st.slider('Shade Factor', 0.0, 0.1, 0.01, 0.01)
+    grayscale_sketch, color_sketch = cv2.pencilSketch(
+        img,
+        sigma_s=sigma_s,
+        sigma_r=sigma_r,
+        shade_factor=shade_factor
+    )
+    return grayscale_sketch if sketch_option == 'Grayscale' else color_sketch
+
+
+# may have better results with blurred image
+def stylization_effect(img):
+    sigma_s = st.slider('Sigma S', 0.0, 200.0, 60.0)
+    sigma_r = st.slider('Sigma R', 0.0, 1.0, 0.45, 0.01)
+    return cv2.stylization(img, sigma_s=sigma_s, sigma_r=sigma_r)
